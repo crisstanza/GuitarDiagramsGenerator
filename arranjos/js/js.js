8a979b21d9a1a51a255c26af04724c5a3a9c7ecc
Cris Stanza <crisstanza@users.noreply.github.com>
2020-12-04 18:33:32 -0300

    Regex support for URL parameters.

--- arranjos/js/js.js
+++ arranjos/js/js.js
@@ -5,7 +5,7 @@
 	let GuitarDiagramsGenerator = io.github.crisstanza.GuitarDiagramsGenerator;
 
 	function init(event) {
-		document.body.setAttribute('data-arrangement', document.location.href.replace(/.*\/arranjos\/(.*)\/(index\.html)?/, '$1'));
+		document.body.setAttribute('data-arrangement', document.location.href.replace(/.*\/arranjos\/(.*)\/(index\.html)?.*/, '$1'));
 		GuitarDiagramsGenerator.reinit();
 	}
 
